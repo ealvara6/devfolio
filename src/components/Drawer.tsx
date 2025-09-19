02f785c2a75b655ed02d297f3f1d6b48d6d4b330
ealvara6 <50152010+ealvara6@users.noreply.github.com>
2025-09-18 22:10:56 -0500

    fix(drawer): add sticky positioning and prevent scroll when opened (#6)
    * fix(drawer): add sticky positioning
    
    * fix(drawer): prevent scrolling while drawer is open

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -2,6 +2,7 @@ import { XMarkIcon } from '@heroicons/react/24/outline';
 import { links } from '../data/navLinks';
 import { Transition, TransitionChild } from '@headlessui/react';
 import { ThemeToggle } from './ThemeToggle';
+import { useEffect } from 'react';
 
 type DrawerProps = {
   open: boolean;
@@ -11,11 +12,20 @@ type DrawerProps = {
 export const Drawer = ({ open, onClose }: DrawerProps) => {
   const delays = ['delay-[300ms]', 'delay-[600ms]', 'delay-[900ms]'];
 
+  useEffect(() => {
+    if (open) document.body.style.overflow = 'hidden';
+    else document.body.style.overflow = '';
+
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [open]);
+
   return (
     <div
       className={`absolute top-0  bg-surface min-h-screen min-w-screen -translate-y-full sm:hidden z-50 ${
         open ? 'translate-y-0' : '-translate-y-full'
-      } transition-all duration-700 p-3 gap-10 flex flex-col`}
+      } transition-all duration-700 p-3 gap-10 flex flex-col sticky`}
     >
       <div className=" flex justify-between">
         <button onClick={onClose}>
