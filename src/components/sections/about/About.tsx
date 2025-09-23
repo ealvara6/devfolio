75d540b347fa430db9d3f8bc3e081ee0c9b035cb
Eduardo Alvarado <ealvara73@gmail.com>
2025-09-22 21:52:06 -0500

    feature(about): add full name

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -3,9 +3,16 @@ import headShot from '@/assets/headshot-no-background.png';
 export const About = () => {
   return (
     <section
-      className="min-h-screen scroll-mt-20 flex justify-center pt-24"
+      className="relative min-h-screen scroll-mt-20 flex justify-center pt-24 w-full max-w-3xl mx-auto"
       id="about"
     >
+      <div className="absolute tracking-widest text-6xl top-64 left-4 font-name font-bold">
+        Eduardo
+      </div>
+      <div className="absolute tracking-widest text-6xl top-64 right-0 font-name font-bold">
+        {' '}
+        Alvarado
+      </div>
       <img
         src={headShot}
         alt="head shot of Eduardo Alvarado"
