31c0b5144977ea9169f7c5656bbc604aa363e61e
Eduardo Alvarado <ealvara73@gmail.com>
2025-09-22 21:21:23 -0500

    feature(home): add headshot photo to about page

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -1,7 +1,16 @@
+import headShot from '@/assets/headshot-no-background.png';
+
 export const About = () => {
-    return (
-        <section className="min-h-screen scroll-mt-20" id="about">
-            <div>About</div>
-        </section>
-    )
-}
+  return (
+    <section
+      className="min-h-screen scroll-mt-20 flex justify-center pt-24"
+      id="about"
+    >
+      <img
+        src={headShot}
+        alt="head shot of Eduardo Alvarado"
+        className="h-full mask-b-from-90% mask-b-to-100%"
+      />
+    </section>
+  );
+};
