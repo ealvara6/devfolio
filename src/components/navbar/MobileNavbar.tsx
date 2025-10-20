827ff5e24e0411cc52ee57d6aca30df98e3f3db7
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-20 18:17:27 -0500

    style(navbar): update font and color of menu component

--- src/components/navbar/MobileNavbar.tsx
+++ src/components/navbar/MobileNavbar.tsx
@@ -2,8 +2,10 @@ import { Bars3Icon } from '@heroicons/react/24/outline';
 
 export const MobileNavbar = () => {
   return (
-    <button className="flex min-w-34 sm:hidden items-center justify-between py-2 px-3 rounded-full min-h-10 border bg-white/5 backdrop-blur-md shadow-lg shadow-black/40">
-      <div className="text-md tracking-wider font-medium">Alvarado</div>
+    <button className="flex min-w-36 sm:hidden items-center justify-between py-3 px-3 rounded-full min-h-10 border border-border bg-white/5 backdrop-blur-md shadow-lg shadow-black/40">
+      <div className="text-md tracking-wider font-heading font-medium">
+        Alvarado
+      </div>
       <Bars3Icon className="w-6" />
     </button>
   );
