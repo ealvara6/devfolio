b899901b119830fe390df174c0a3466a959f3cd5
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-20 17:24:46 -0500

    chore(navbar): remove old mobile navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { useCallback, useState } from 'react';
 import { Navbar } from './components/Navbar';
-import { Drawer } from './components/Drawer';
 import { Sections } from './components/sections/Sections';
 
 export const App = () => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const openDrawer = useCallback(() => setDrawerOpen(true), []);
-  const closeDrawer = useCallback(() => setDrawerOpen(false), []);
-
   return (
     <main className=" bg-bg text-text min-h-screen transition-all duration-500 relative">
-      <Navbar openMenu={openDrawer} />
-      <Drawer open={drawerOpen} onClose={closeDrawer} />
+      <Navbar />
       <Sections />
     </main>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,9 @@
-import { Bars3Icon, EnvelopeIcon } from '@heroicons/react/24/outline';
+import { EnvelopeIcon } from '@heroicons/react/24/outline';
 import { ThemeToggle } from './ThemeToggle';
 import { useEffect, useState } from 'react';
 import { links, type ActiveProps } from '../data/navLinks';
 
-type NavbarProps = { openMenu: () => void };
-
-export const Navbar = ({ openMenu }: NavbarProps) => {
+export const Navbar = () => {
   const [isActive, setIsActive] = useState<ActiveProps>('');
   const [scrolled, setScrolled] = useState(false);
 
@@ -19,29 +17,16 @@ export const Navbar = ({ openMenu }: NavbarProps) => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  const Menu = () => {
-    return (
-      <button className="col-start-1 sm:hidden justify-self-start">
-        <Bars3Icon
-          aria-label="Open Menu"
-          className="w-10 text-accent bg-bg dark:shadow-none shadow-md p-1 rounded-full"
-          onClick={openMenu}
-        />
-      </button>
-    );
-  };
-
   return (
     <nav
-      className={`min-h-14 items-center grid p-2 grid-cols-[1fr_2fr_1fr] justify-items-center font-sans fixed top-0 left-0 min-w-full z-10 transition-shadow  ${
+      className={`min-h-14 items-center hidden sm:grid p-2 grid-cols-[1fr_2fr_1fr] justify-items-center font-sans fixed top-0 left-0 min-w-full z-10 transition-shadow  ${
         scrolled ? 'backdrop-blur shadow-md' : 'backdrop-blur-none'
       }`}
     >
-      <Menu />
-      <div className=" justify-self-center sm:justify-self-start font-heading text-3xl sm:text-4xl font-bold col-start-2 sm:col-start-1">
+      <div className="justify-self-start font-heading text-4xl font-bold :col-start-1">
         Alvarado
       </div>
-      <ul className="justify-self-center hidden sm:justify-center md:gap-10  sm:flex w-full">
+      <ul className="justify-self-center justify-center md:gap-10  flex w-full">
         {links.map((link) => (
           <li
             key={link.name}
@@ -65,7 +50,7 @@ export const Navbar = ({ openMenu }: NavbarProps) => {
         <button className="border cursor-pointer shadow-md hover:-translate-y-0.5 text-sm hover:translate-x-0.5 transition border-border  p-3 rounded-full bg-accent hover:bg-accent-hover active:translate-0">
           <EnvelopeIcon className="w-6 h-6 text-white" aria-label="Contact" />
         </button>
-        <ThemeToggle className="hidden sm:block" variant="bg2" />
+        <ThemeToggle className="block" variant="bg2" />
       </div>
     </nav>
   );
