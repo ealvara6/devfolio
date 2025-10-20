0d91410d754e54522a161bad41943c0582b52357
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-20 18:40:59 -0500

    feat(hero): add background image

--- src/components/sections/Sections.tsx
+++ src/components/sections/Sections.tsx
@@ -1,10 +1,12 @@
 import { About } from './about/About';
 import { Contact } from './contact/Contact';
+import { Hero } from './hero/Hero';
 import { Projects } from './projects/Projects';
 
 export const Sections = () => {
   return (
-    <main className="pt-16">
+    <main className="pb-16">
+      <Hero />
       <About />
       <Projects />
       <Contact />
---
+++ src/components/sections/hero/Hero.tsx
@@ -0,0 +1,7 @@
+export const Hero = () => {
+  return (
+    <div className="min-h-screen w-full bg-[url('../assets/Hexagon.svg')]">
+      Hero
+    </div>
+  );
+};
