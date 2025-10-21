e7519745306f08961734f9e2c0c38cc80960f5cd
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-21 13:41:36 -0500

    style(hero): add background fade

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -2,7 +2,8 @@ import Memoji from '@/assets/Memoji.png';
 
 export const Hero = () => {
   return (
-    <div className="min-h-screen w-full bg-[url('../assets/Hexagon.svg')]">
+    <div className="min-h-screen w-full bg-[url('../assets/Hexagon.svg')] relative z-10 ">
+      <div className="absolute inset-0 bg-black/60 -z-10"></div>
       <div className="mx-7 pt-24 flex flex-col gap-4 justify-center items-center text-text">
         <div className="border w-72 h-72 bg-border border-border rounded-full flex justify-center items-center">
           <img src={Memoji} alt="Image of Memoji" className="w-46 h-46" />
