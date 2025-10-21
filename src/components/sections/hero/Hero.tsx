a04431127829934ccb206a989ad2173920a9bf7c
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-21 13:36:07 -0500

    feat(hero): add content to hero section

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -1,7 +1,29 @@
+import Memoji from '@/assets/Memoji.png';
+
 export const Hero = () => {
   return (
     <div className="min-h-screen w-full bg-[url('../assets/Hexagon.svg')]">
-      Hero
+      <div className="mx-7 pt-24 flex flex-col gap-4 justify-center items-center text-text">
+        <div className="border w-72 h-72 bg-border border-border rounded-full flex justify-center items-center">
+          <img src={Memoji} alt="Image of Memoji" className="w-46 h-46" />
+        </div>
+        <div className="flex flex-col text-center gap-1">
+          <div className="font-heading text-3xl font-bold tracking-wider">
+            Hi, I'm Eduardo
+          </div>
+          <div className="font-subheading text-lg font-medium text-text-muted tracking-wide">
+            Full-Stack Developer & Tech Enthusiast
+          </div>
+        </div>
+        <div className="w-full text-xl flex flex-col gap-4 tracking-wide">
+          <button className="rounded-md w-full py-4 bg-accent">
+            View My Work
+          </button>
+          <button className="rounded-md w-full py-4 border border-accent ">
+            Contact Me
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
