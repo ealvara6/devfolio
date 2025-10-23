00af6d1cf8110f829f0630e62ba0cd7bc8794491
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-22 20:56:12 -0500

    feat(navbar): fixed navbar to top of the viewport

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Sections } from './components/sections/Sections';
 export const App = () => {
   return (
     <main className=" bg-bg text-text min-h-screen transition-all duration-500 relative">
-      <nav className="sm:min-h-16 min-h-14 flex items-center py-6 px-3 justify-end">
+      <nav className="sm:min-h-16 min-h-14 flex items-center py-6 px-3 justify-end fixed z-50 w-full top-0 left-0">
         <Navbar />
         <MobileNavbar />
       </nav>
