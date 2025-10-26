3b29b2be9fb9d02f698a840210e3c866da1497a8
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-26 15:01:04 -0500

    feat(about): add headshot photo

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -1,7 +1,18 @@
+import headShot from '@/assets/headShot.png';
+
 export const About = () => {
   return (
-    <section className="h-screen" id="about">
-      About section
+    <section
+      className="h-screen flex flex-col items-center pt-14 mx-7"
+      id="about"
+    >
+      <div className="w-full h-fit bg-surface rounded-xl">
+        <img
+          src={headShot}
+          alt="head shot of Eduardo Alvarado"
+          className="p-6 rounded-4xl"
+        />
+      </div>
     </section>
   );
 };
