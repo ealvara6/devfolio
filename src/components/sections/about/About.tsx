322b050fdd04ad4470c9dc0a60bd36c7a3fe4cc3
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-26 14:34:46 -0500

    feat(about): remove old about section

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -1,23 +1,7 @@
-import headShot from '@/assets/headshot-no-background.png';
-
 export const About = () => {
   return (
-    <section
-      className="relative min-h-screen scroll-mt-20 flex justify-center pt-24 w-full max-w-3xl mx-auto"
-      id="about"
-    >
-      <div className="absolute tracking-widest text-6xl top-64 left-4 font-name font-bold">
-        Eduardo
-      </div>
-      <div className="absolute tracking-widest text-6xl top-64 right-0 font-name font-bold">
-        {' '}
-        Alvarado
-      </div>
-      <img
-        src={headShot}
-        alt="head shot of Eduardo Alvarado"
-        className="h-full mask-b-from-90% mask-b-to-100%"
-      />
+    <section className="h-screen" id="about">
+      About section
     </section>
   );
 };
