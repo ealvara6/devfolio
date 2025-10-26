2d9e321c110e8bd9a2b4d61994d1fa1c06e86df2
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-26 14:01:17 -0500

    style(hero): adjust wave emoji size

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -10,7 +10,8 @@ export const Hero = () => {
         </div>
         <div className="flex flex-col text-center gap-1">
           <div className="font-heading text-3xl font-medium tracking-wider relative pb-2">
-            Hi, I'm Eduardo<span className="absolute">&#x1F44B;</span>
+            Hi, I'm Eduardo
+            <span className="absolute text-2xl translate-y-0.5">&#x1F44B;</span>
           </div>
           <div className="font-subheading text-lg font-light text-text-muted tracking-wide pb-6">
             Full-Stack Developer & Tech Enthusiast
