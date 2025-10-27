250a21b4c315b25b17c3dce91262366858c13de5
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-27 12:27:48 -0500

    feat(about): add name and title

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -7,11 +7,21 @@ export const About = () => {
       id="about"
     >
       <div className="w-full h-fit bg-surface rounded-xl">
-        <img
-          src={headShot}
-          alt="head shot of Eduardo Alvarado"
-          className="p-6 rounded-4xl"
-        />
+        <div className="p-6 flex flex-col items-center gap-2">
+          <img
+            src={headShot}
+            alt="head shot of Eduardo Alvarado"
+            className="rounded-4xl"
+          />
+          <div className="text-center">
+            <div className="text-xl tracking-wide font-semibold">
+              Eduardo Alvarado
+            </div>
+            <div className="text-base text-text-muted font-medium">
+              Jr. Full Stack Web Developer
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
