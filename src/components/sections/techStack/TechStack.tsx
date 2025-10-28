14160129dda8f909654188829cb61022e01ec3c8
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-28 14:15:04 -0500

    feat(tech): add tech card section framework

--- src/components/sections/techStack/TechStack.tsx
+++ src/components/sections/techStack/TechStack.tsx
@@ -1,3 +1,34 @@
+import type { ReactNode } from 'react';
+
+const techSection = [
+  {
+    name: 'Frontend',
+  },
+  {
+    name: 'Backend',
+  },
+  {
+    name: 'Database',
+  },
+  {
+    name: 'Tools',
+  },
+];
+
 export const TechStack = () => {
-  return <section className="min-h-screen">Technology</section>;
+  return (
+    <section className="min-h-screen flex flex-col mx-7">
+      <h1 className="self-center text-2xl font-bold tracking-wider">
+        Technology
+      </h1>
+      {techSection.map((tech) => {
+        return (
+          <div className="min-h-[20vh] flex flex-col">
+            <h2 className="text-lg tracking-wide pb-1">{tech.name}</h2>
+            <div className="border border-accent grow rounded-md p-2"></div>
+          </div>
+        );
+      })}
+    </section>
+  );
 };
