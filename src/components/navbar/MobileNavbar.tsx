35aacda731cc39b25299cc316e51023ac2b96040
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-27 19:18:15 -0500

    style(navbar): remove border

--- src/components/navbar/MobileNavbar.tsx
+++ src/components/navbar/MobileNavbar.tsx
@@ -2,7 +2,7 @@ import { Bars3Icon } from '@heroicons/react/24/outline';
 
 export const MobileNavbar = () => {
   return (
-    <button className="flex min-w-36 sm:hidden items-center justify-between py-3 px-3 rounded-full min-h-10 border border-border bg-white/5 backdrop-blur-md shadow-lg shadow-black/40">
+    <button className="flex min-w-36 sm:hidden items-center justify-between py-3 px-3 rounded-full min-h-10 bg-white/5 backdrop-blur-md shadow-lg shadow-black/40">
       <div className="text-md tracking-wider font-heading font-medium">
         Alvarado
       </div>
