3cb4a158936f3ab01f1d3b8a70856657ee79b4bf
Eduardo Alvarado <ealvara73@gmail.com>
2025-10-28 14:27:41 -0500

    feat(tech): add tech card frame

--- src/components/sections/techStack/TechStack.tsx
+++ src/components/sections/techStack/TechStack.tsx
@@ -3,15 +3,39 @@ import type { ReactNode } from 'react';
 const techSection = [
   {
     name: 'Frontend',
+    tech: [
+      {
+        name: 'React',
+        img: 'image',
+      },
+    ],
   },
   {
     name: 'Backend',
+    tech: [
+      {
+        name: 'React',
+        img: 'image',
+      },
+    ],
   },
   {
     name: 'Database',
+    tech: [
+      {
+        name: 'React',
+        img: 'image',
+      },
+    ],
   },
   {
     name: 'Tools',
+    tech: [
+      {
+        name: 'React',
+        img: 'image',
+      },
+    ],
   },
 ];
 
@@ -21,11 +45,19 @@ export const TechStack = () => {
       <h1 className="self-center text-2xl font-bold tracking-wider">
         Technology
       </h1>
-      {techSection.map((tech) => {
+      {techSection.map((techSection) => {
         return (
           <div className="min-h-[20vh] flex flex-col">
-            <h2 className="text-lg tracking-wide pb-1">{tech.name}</h2>
-            <div className="border border-accent grow rounded-md p-2"></div>
+            <h2 className="text-lg tracking-wide pb-1">{techSection.name}</h2>
+            <div className="border border-accent grow rounded-md p-2">
+              {techSection.tech.map((tech) => {
+                return (
+                  <div className="w-20 h-20 border flex flex-col justify-center gap-2 border-border rounded-md items-center">
+                    <div>{tech.img}</div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         );
       })}
