848393a0fb8e3208f2ac2b456be829037a1c3f68
Eduardo Alvarado <ealvara73@gmail.com>
2025-11-03 18:07:07 -0600

    style(tech): small style changes
    style(tech): style changes

--- src/components/sections/techStack/TechStack.tsx
+++ src/components/sections/techStack/TechStack.tsx
@@ -4,15 +4,19 @@ import { TECH_REGISTRY } from './tech';
 
 export const TechStack = () => {
   return (
-    <section className="min-h-screen flex flex-col mx-7 gap-10">
-      <h1 className="self-center text-2xl font-bold tracking-wider">
+    <section className="min-h-screen flex flex-col mx-7 gap-22 pt-14">
+      <h1 className="self-center text-3xl font-bold tracking-widest">
         Technology
       </h1>
       {SECTIONS.map((section) => {
         return (
           <div className="min-h-[20vh] flex flex-col" key={section.key}>
-            <h2 className="text-lg tracking-wide pb-1">{section.name}</h2>
-            <div className="border border-accent grow rounded-md py-10 px-4 flex gap-y-6 gap-x-8 flex-wrap justify-center ">
+            <h2 className="text-xl tracking-wider pb-3 self-center font-semibold text-accent-light/90">
+              {section.name}
+            </h2>
+            <div className="relative grow rounded-md py-10 px-4 flex gap-y-4 gap-x-5 flex-wrap justify-center">
+              <div className="h-full rounded-md absolute w-full inset-0 z-10 bg-accent bg-gradient-to-b from-emerald-400 via-teal-400 to-cyan-500" />
+              <div className="inset-[2px] bg-bg rounded-md absolute z-10" />
               {section.tech.map((techKey) => {
                 const tech = TECH_REGISTRY[techKey];
                 const from = withOpacity(tech.baseColor, 0.18);
@@ -20,7 +24,7 @@ export const TechStack = () => {
                 return (
                   <div
                     key={tech.key}
-                    className={`w-20 h-20 border flex flex-col justify-center gap-2 border-border rounded-md items-center`}
+                    className={`w-20 h-20 flex flex-col justify-center rounded-md items-center z-20`}
                     style={{
                       background: `linear-gradient(to bottom, ${from}, ${to})`,
                     }}
