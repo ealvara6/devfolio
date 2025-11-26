4e462fb6275f9626154c406f705b1a3456a5165b
Eduardo Alvarado <ealvara73@gmail.com>
2025-11-25 20:25:11 -0600

    (feat):contact: add basic contact form structure

--- src/components/sections/contact/Contact.tsx
+++ src/components/sections/contact/Contact.tsx
@@ -1,16 +1,49 @@
 import { SubHeading } from '@/components/shared/SubHeading';
+import { Button, Fieldset, Input, Textarea } from '@headlessui/react';
+import githubSvg from '@/assets/github.svg';
+import linkedinSvg from '@/assets/linkedin.svg';
+import { EnvelopeIcon } from '@heroicons/react/24/outline';
 
 export const Contact = () => {
   return (
     <section
-      className="min-h-screen scroll-mt-20 mt-14 flex flex-col mx-7 text-center"
+      className="min-h-screen scroll-mt-20 mt-14 flex flex-col gap-10 mx-7 text-center"
       id="contact"
     >
-      <SubHeading>Get In Touch</SubHeading>
-      <div className="mt-4 tracking-wide font-light">
-        I'm actively growing my career as a full-stack web developer and open to
-        opportunities. Reach out anytime!
+      <div>
+        <SubHeading>Get In Touch</SubHeading>
+        <div className="mt-4 tracking-wide font-light">
+          I'm always open to new opportunities, collaborations, or questions
+          about my work. Feel free to reach out!
+        </div>
       </div>
+      <div className="flex justify-center gap-12">
+        <img src={githubSvg} alt="Github Icon" className="w-10" />
+        <img src={linkedinSvg} alt="Linkedin Icon" className="w-10" />
+        <EnvelopeIcon className="w-10" />
+      </div>
+      <Fieldset className="flex flex-col gap-8">
+        <Input
+          name="name"
+          type="text"
+          className="px-3 py-3.5 rounded-md bg-surface"
+          placeholder="Full Name"
+        />
+        <Input
+          name="email"
+          type="text"
+          className="px-3 py-3.5 rounded-md bg-surface"
+          placeholder="Email"
+        />
+        <Textarea
+          name="message"
+          className="px-3 py-3.5 rounded-md bg-surface resize-none h-48"
+          placeholder="Message"
+        />
+        <Button className="bg-accent p-3.5 rounded-xl text-xl font-semibold tracking-wider">
+          Submit
+        </Button>
+      </Fieldset>
     </section>
   );
 };
