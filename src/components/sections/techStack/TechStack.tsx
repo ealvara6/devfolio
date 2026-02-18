4fb91db7c15cc7a5342eba98ec7e245b9c2dbd70
Eduardo Alvarado <ealvara73@gmail.com>
2026-02-17 18:08:32 -0600

    feat(tech): adjust layout and width of tech stack cards

--- src/components/sections/techStack/TechStack.tsx
+++ src/components/sections/techStack/TechStack.tsx
@@ -5,15 +5,23 @@ import { SubHeading } from '@/components/shared/SubHeading';
 
 export const TechStack = () => {
   return (
-    <section className="min-h-screen flex flex-col mx-7 gap-22 pt-14">
-      <SubHeading className="self-center">Technology</SubHeading>
-      {SECTIONS.map((section) => {
+    <section className="flex flex-col mx-7 gap-22 pt-14 lg:grid lg:grid-cols-2 lg:grid-rows-[100px_1fr_1fr] lg:items-center lg:gap-y-10 mb-14">
+      <SubHeading className="self-center col-span-2 text-center">
+        Technology
+      </SubHeading>
+      {SECTIONS.map((section, index) => {
+        console.log(SECTIONS.length, index);
         return (
-          <div className="min-h-[20vh] flex flex-col" key={section.key}>
+          <div
+            className={`min-h-sm flex flex-col items-center ${
+              SECTIONS.length - 1 == index && 'lg:col-span-2'
+            }`}
+            key={section.key}
+          >
             <h2 className="text-xl tracking-wider pb-3 self-center font-semibold text-accent-light/90">
               {section.name}
             </h2>
-            <div className="relative grow rounded-md py-10 px-4 flex gap-y-4 gap-x-5 flex-wrap justify-center">
+            <div className="relative rounded-md py-10 min-h-72 px-4 flex items-center max-w-lg lg:max-w-xl w-full gap-y-2 gap-x-5 flex-wrap justify-center">
               <div className="h-full rounded-md absolute w-full inset-0 z-10 bg-accent bg-gradient-to-b from-emerald-400 via-teal-400 to-cyan-500" />
               <div className="inset-[2px] bg-bg rounded-md absolute z-10" />
               {section.tech.map((techKey) => {
