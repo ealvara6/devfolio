945e1c60b8b3f070eaa646b5813709f308ee8ce9
Eduardo Alvarado <ealvara73@gmail.com>
2026-03-04 19:37:19 -0600

    feat: add option for button to be a link component

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -7,10 +7,12 @@ type ButtonProps = {
   children: React.ReactNode;
   className?: string;
   variant?: ButtonVariant;
+  href?: string;
+  target?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const baseClasses =
-  'border rounded-md border-accent hover:-translate-y-0.5 hover:translate-x-0.5 transition-all shadow-md cursor-pointer py-3 active:translate-0 tracking-wider';
+  'border rounded-md text-center border-accent hover:-translate-y-0.5 hover:translate-x-0.5 transition-all shadow-md cursor-pointer py-3 active:translate-0 tracking-wider';
 
 const variantClasses: Record<ButtonVariant, string> = {
   primary: 'bg-gradient-to-r from-emerald-500 to-emerald-400',
@@ -21,14 +23,28 @@ export const Button = ({
   children,
   className,
   variant = 'primary',
+  href,
+  target,
   ...props
 }: ButtonProps) => {
-  return (
-    <button
-      className={clsx(baseClasses, variantClasses[variant], className)}
-      {...props}
-    >
-      {children}
-    </button>
-  );
+  if (href) {
+    return (
+      <a
+        href={href}
+        target={target}
+        className={clsx(baseClasses, variantClasses[variant], className)}
+      >
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <button
+        className={clsx(baseClasses, variantClasses[variant], className)}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
 };
