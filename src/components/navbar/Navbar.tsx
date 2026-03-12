9a1748d9c9589e5023c75232cc008d2fff8ce31f
Eduardo Alvarado <ealvara73@gmail.com>
2026-03-11 20:03:08 -0500

    remove mail button from navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import { EnvelopeIcon } from '@heroicons/react/24/outline';
 import { useEffect, useState } from 'react';
 import { links } from '../../data/navLinks';
 import clsx from 'clsx';
@@ -21,7 +20,7 @@ export const Navbar = ({ className }: { className?: string }) => {
   return (
     <div
       className={clsx(
-        `min-h-14 items-center p-2 grid-cols-[1fr_2fr_1fr] justify-items-center font-sans fixed top-0 left-0 min-w-full z-10 transition-shadow  ${
+        `min-h-14 items-center p-2 px-12 grid-cols-[1fr_2fr_1fr] justify-items-center font-sans fixed top-0 left-0 min-w-full z-10 transition-shadow  ${
           scrolled ? 'backdrop-blur shadow-md' : 'backdrop-blur-none'
         }`,
         className,
@@ -50,9 +49,9 @@ export const Navbar = ({ className }: { className?: string }) => {
         ))}
       </ul>
       <div className="justify-self-end flex gap-6 items-center col-start-3">
-        <button className="border cursor-pointer shadow-md hover:-translate-y-0.5 text-sm hover:translate-x-0.5 transition border-border  p-3 rounded-full bg-accent hover:bg-accent-hover active:translate-0">
+        {/* <button className="border cursor-pointer shadow-md hover:-translate-y-0.5 text-sm hover:translate-x-0.5 transition border-border  p-3 rounded-full bg-accent hover:bg-accent-hover active:translate-0">
           <EnvelopeIcon className="w-6 h-6 text-white" aria-label="Contact" />
-        </button>
+        </button> */}
       </div>
     </div>
   );
