e0a8ab758c769fcb1e561f5eaeafb545057b38be
Eduardo Alvarado <ealvara73@gmail.com>
2026-03-17 15:46:02 -0500

    style(hero): change hero size on mobile screens

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -8,20 +8,26 @@ export const Hero = () => {
       id="Hero"
     >
       <div className="absolute inset-0 bg-black/10 -z-10 bg-gradient-to-b from-transparent to-bg pointer-events-none"></div>
-      <div className="mx-7 pt-24 flex flex-col gap-2 justify-center items-center text-text">
-        <div className="border w-64 h-64 bg-surface border-border rounded-full flex justify-center items-center">
-          <img src={Memoji} alt="Image of Memoji" className="w-36 h-auto" />
+      <div className="mx-7 pt-64 flex flex-col gap-2 justify-center items-center text-text">
+        <div className="border w-48 h-48 md:w-64 md:h-64 bg-surface border-border rounded-full flex justify-center items-center">
+          <img
+            src={Memoji}
+            alt="Image of Memoji"
+            className="w-24 md:w-36 h-auto"
+          />
         </div>
         <div className="flex flex-col text-center">
-          <div className="font-heading text-3xl font-medium tracking-wider relative">
+          <div className="font-heading text-2xl md:text-3xl font-medium tracking-wider relative">
             Hi, I'm Eduardo
-            <span className="absolute text-2xl translate-y-0.5">&#x1F44B;</span>
+            <span className="absolute text-xl md:text-2xl translate-y-0.5">
+              &#x1F44B;
+            </span>
           </div>
           <div className="font-subheading text-lg font-light text-text-muted tracking-normal pb-4">
             Full-Stack Developer & Tech Enthusiast
           </div>
         </div>
-        <div className="w-full text-lg flex flex-col gap-6 tracking-wide items-center">
+        <div className="w-full text-base md:text-lg flex flex-col gap-6 tracking-wide items-center">
           <Button href="#Projects" className="w-full max-w-2xl">
             View My Work
           </Button>
