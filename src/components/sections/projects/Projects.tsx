050cdf3d600695fa12cce175acffdf80e08f8073
Eduardo Alvarado <ealvara73@gmail.com>
2026-03-25 20:16:16 -0500

    feat(project): add fade in animation to each project card

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -8,15 +8,15 @@ export const Projects = () => {
     <div className="min-h-fit pt-30 pb-30 px-7 mx-auto" id="Projects">
       <div className="flex flex-col gap-14">
         <SubHeading className="self-center">Projects</SubHeading>
-        <div className="flex flex-col xl:flex-row gap-8 items-center xl:justify-center">
-          {projectData.map((project, index) => {
+        <FadeInSection className="flex flex-col xl:flex-row gap-8 items-center xl:justify-center">
+          {projectData.map((project) => {
             return (
-              <FadeInSection delay={`delay-${index * 500}`}>
+              <FadeInSection>
                 <ProjectCard data={project} />
               </FadeInSection>
             );
           })}
-        </div>
+        </FadeInSection>
       </div>
     </div>
   );
