c35abd5e1d0294aa84550c9d32de53217787d7b4
Eduardo Alvarado <ealvara73@gmail.com>
2026-04-01 18:55:16 -0500

    fix(hero): fix issue with background fading in on load

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -4,43 +4,45 @@ import { FadeInSection } from '@/components/shared/FadeInSection';
 
 export const Hero = () => {
   return (
-    <FadeInSection
-      className="min-h-[90vh] flex justify-center w-full bg-[url('../assets/Hexagon.svg')] lg: relative z-10"
+    <section
+      className="min-h-[90vh] w-full bg-[url('../assets/Hexagon.svg')] relative z-10 flex justify-center"
       id="Hero"
     >
       <div className="absolute inset-0 bg-black/10 -z-10 bg-gradient-to-b from-transparent to-bg pointer-events-none"></div>
-      <div className="mx-7 w-full  md:max-w-7xl flex flex-col gap-2 justify-center items-center text-text">
-        <div className="border w-48 h-48 md:w-64 md:h-64 bg-surface border-border rounded-full flex justify-center items-center">
-          <img
-            src={Memoji}
-            alt="Image of Memoji"
-            className="w-24 md:w-36 h-auto"
-          />
-        </div>
-        <div className="flex flex-col text-center">
-          <div className="font-heading text-2xl md:text-3xl font-medium tracking-wider relative">
-            Hi, I'm Eduardo
-            <span className="absolute text-xl md:text-2xl translate-y-0.5">
-              &#x1F44B;
-            </span>
+      <FadeInSection className=" flex justify-center w-full">
+        <div className="mx-7 w-full  md:max-w-7xl flex flex-col gap-2 justify-center items-center text-text">
+          <div className="border w-48 h-48 md:w-64 md:h-64 bg-surface border-border rounded-full flex justify-center items-center">
+            <img
+              src={Memoji}
+              alt="Image of Memoji"
+              className="w-24 md:w-36 h-auto"
+            />
           </div>
-          <div className="font-subheading text-lg font-light text-text-muted tracking-normal pb-4">
-            Full-Stack Developer & Tech Enthusiast
+          <div className="flex flex-col text-center">
+            <div className="font-heading text-2xl md:text-3xl font-medium tracking-wider relative">
+              Hi, I'm Eduardo
+              <span className="absolute text-xl md:text-2xl translate-y-0.5">
+                &#x1F44B;
+              </span>
+            </div>
+            <div className="font-subheading text-lg font-light text-text-muted tracking-normal pb-4">
+              Full-Stack Developer & Tech Enthusiast
+            </div>
+          </div>
+          <div className="w-full text-base md:text-lg flex flex-col gap-6 tracking-wide items-center">
+            <Button href="#Project" className="w-full max-w-2xl">
+              View My Work
+            </Button>
+            <Button
+              className="w-full max-w-2xl"
+              href="#Contact"
+              variant="outline"
+            >
+              Contact Me
+            </Button>
           </div>
         </div>
-        <div className="w-full text-base md:text-lg flex flex-col gap-6 tracking-wide items-center">
-          <Button href="#Projects" className="w-full max-w-2xl">
-            View My Work
-          </Button>
-          <Button
-            className="w-full max-w-2xl"
-            href="#Contact"
-            variant="outline"
-          >
-            Contact Me
-          </Button>
-        </div>
-      </div>
-    </FadeInSection>
+      </FadeInSection>
+    </section>
   );
 };
