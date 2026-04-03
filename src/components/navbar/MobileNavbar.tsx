7fe482f5c382b5d4b58c993438d18da1b67f0d4a
Eduardo Alvarado <ealvara73@gmail.com>
2026-04-02 20:10:07 -0500

    style(navbar): change icon and animation on mobile nav

--- src/components/navbar/MobileNavbar.tsx
+++ src/components/navbar/MobileNavbar.tsx
@@ -1,4 +1,3 @@
-import { Bars3Icon } from '@heroicons/react/24/outline';
 import {
   DropdownMenuTrigger,
   DropdownMenu,
@@ -8,6 +7,7 @@ import {
 } from '../ui/dropdown-menu';
 import { Button } from '../ui/button';
 import { links } from '@/data/navLinks';
+import { ChevronDown } from 'lucide-react';
 
 export const MobileNavbar = ({ className }: { className: string }) => {
   return (
@@ -16,7 +16,7 @@ export const MobileNavbar = ({ className }: { className: string }) => {
         <DropdownMenuTrigger asChild>
           <Button className="flex gap-4 min-w-36 text-lg tracking-wide group rounded-full min-h-12 bg-white/5 backdrop-blur-md shadow-lg shadow-black/40">
             Alvarado
-            <Bars3Icon className="scale-150 transition-transform duration-300 group-data-[state=open]:rotate-90" />
+            <ChevronDown className="scale-150 transition-transform duration-300 ease-out group-data-[state=open]:rotate-180" />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="bg-white/5 backdrop-blur-md shadow-lg shadow-black/40 border-none text-white">
